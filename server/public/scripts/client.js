40c4e292bbc6435dedc18e5582be81d9ae596333
Jason Inouye <jasonjinouye@gmail.com>
2022-03-11 16:29:12 -0600

    added button listener, tested post of single string of data

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,25 @@ $(readyNow);
 
 function readyNow(){
     console.log( 'jQuery loaded' );
+    $('#calcBtn').on( 'click', handleSubmit )
 };
+
+
+function handleSubmit(){
+    console.log( 'inside the handleSubmit' );
+
+    let mathQ = $('#mathQ').val();
+
+
+    $.ajax({
+        url: '/mathQuery',
+        method: 'POST',
+        data: {
+            mathQ: mathQ
+        }
+    }).then( function(response){
+        console.log(response);
+    })
+
+
+}
--- server/server.js
+++ server/server.js
@@ -4,8 +4,21 @@ const bodyParser = require( 'body-parser');
 const app = express();
 const PORT = 5000;
 
+let mathQuery = [];
+
 app.use( express.static( 'server/public' ));
 app.use( bodyParser.urlencoded({extended: true}));
+
+app.post( '/mathQuery', function( req, res ){
+    console.log( 'POST /mathQuery', req.body );
+
+    mathQuery.push( req.body );
+
+    res.sendStatus(201);
+
+})
+
+
 
 app.listen(PORT, function(){
     console.log( 'server running on port', PORT );
