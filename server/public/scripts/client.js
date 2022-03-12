c232ec15f31a263c23598b84d1add4a854bc1848
Jason Inouye <jasonjinouye@gmail.com>
2022-03-12 09:57:03 -0600

    added logic to capture operator

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,27 +2,52 @@ console.log( 'Client JS' );
 
 $(readyNow);
 
+let passAdd = false;
+let addOperator = false;
+let subtractOperator = false;
+let multipleOperator = false;
+let divideOperator = false;
+
 function readyNow(){
     console.log( 'jQuery loaded' );
-    $('#calcBtn').on( 'click', handleSubmit )
+    $('#addBtn').on( 'click', addOperator )
+    $('#subtractBtn').on( 'click', subtractOperator )
+    $('#multiplyBtn').on( 'click', multipleOperator )
+    $('#divideBtn').on( 'click', divideOperator )
+    $('.opBtn').on('click', handleOperator )
 };
 
+// function addOperator(){
+//     console.log( `inside of addOperator`, passAdd );
+//     return passAdd = true;
+// }
 
 function handleSubmit(){
     console.log( 'inside the handleSubmit' );
 
-    let mathQ = $('#mathQ').val();
-
+    let mathLeft = $('#mathLeft').val();
+    let mathRight = $('#mathRight').val();
 
     $.ajax({
         url: '/mathQuery',
         method: 'POST',
         data: {
-            mathQ: mathQ
+            mathLeft: mathLeft,
+            mathRight: mathRight
         }
     }).then( function(response){
         console.log(response);
     })
+}
 
+function handleOperator(){
+//    console.log( 'inside of handleOperator', $(".opBtn").attr("id") );
+//    console.log( 'inside of handleOperator', $("#addBtn").attr("id") );
+//    console.log( 'inside of handleOperator', $("#subtractBtn").attr("id") );
+//    console.log( 'inside of handleOperator', $("#multiplyBtn").attr("id") );
+//    console.log( 'inside of handleOperator', $("#divideBtn").attr("id") );
 
+   let op = $(this).attr('id');
+   console.log( `The operator is ${op}` );
+    
 }
